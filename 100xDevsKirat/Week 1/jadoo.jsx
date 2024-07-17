00d1a270e5a83a7ce5f5e01512d87d89f6dce9b0
Vaibhav Chauhan <lunaticrealm07@gmail.com>
2024-07-17 18:00:05 +0530

    updated

--- 100xDevsKirat/Week 1/jadoo.jsx
+++
@@ -1,1 +0,0 @@
-console.log("Hello world") therea ae a
---
+++ Sigma Batch(CWH)/57.Loops/index.js
@@ -0,0 +1,44 @@
+// // let a=10
+
+// // // for loop
+// // for(let i =0;i<100;i++)
+// //     {
+// //         console.log(a+i)
+// //     } 
+
+
+// // // for-in loop
+// let obj={
+//     harry:90,
+//      rohan:65,
+//       rahul:99
+// }
+
+// for (c onst a in obj) {
+//     console.log(`marks of ${a} are ${obj[a]}`);    
+// }
+// for(const a in obj){
+//     console.log(obj[a])
+// }
+// // // for of loop
+
+// for (const b of "Raj") {
+//     console.log(b)
+// }
+
+
+// // while loop
+
+let i=5
+// while(i<10)
+//     {
+//         console.log(i)
+//         i++ 
+//     }
+
+// // do while 
+do{
+    console.log(i)
+    i++
+}
+while(i<10)
