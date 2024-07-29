732b91dcc2b0d3c75615aed2a31eced724505d7f
Vaibhav Chauhan <lunaticrealm07@gmail.com>
2024-07-30 03:01:09 +0530

    updated

--- Javascript (by Shraddha)/Lecture 05. Functions & Methods/script.js
+++ Javascript (by Shraddha)/Lecture 05. Functions & Methods/script.js
@@ -53,12 +53,85 @@
 
 
 // // // // // METHODS
-let arr = [1, 2, 3, 4, 5]
-//  arr.forEach(function printVal(val){
-//     console.log(val);
-//  });
+
+// 1. forEach
+// 
+// 
+// let arr = [1, 2, 3, 4, 5]
+// //  arr.forEach(function printVal(val){
+// //     console.log(val);
+// //  });
 
 
-arr.forEach((val) => {
-    console.log(val);
+// arr.forEach((val, idx, arr) => {
+//     console.log(val, idx, arr[2]);
+// })
+
+// Q1: 
+
+// let arr = [1, 2, 3, 4, 5]
+// arr.forEach((val) => {
+//     let sqr = val * val;
+//     console.log(sqr);
+// });
+// 
+
+// 2. Map
+
+// let arr = [1, 2, 3, 4, 5]
+// arr.map((val) => {
+//     console.log(val + 1);
+// })
+// console.log(arr);
+
+
+// let ans = arr.map((val) => {
+//     return val ** 2;
+// })
+// console.log(ans);
+
+// 3.Filter
+
+// let arr = [1, 2, 3, 4, 5, 4]
+// let ans = arr.filter((val) => {
+//     // return val % 2 == 0;
+//     return val > 3;
+// })
+// console.log(ans);
+
+// 4. Reduce
+// let arr = [1, 2, 8, 4, 5]
+
+// let ans = arr.reduce((result, current) => {
+//     return result + current;
+// })
+// console.log(ans);
+
+// Q: largest
+// let ans = arr.reduce((result, current) => {
+// if (result > current) {
+//     current = result;
+// }
+// return current;
+
+//better way
+//     return result > current ? result : current
+// })
+// console.log(ans);
+
+// Q:
+let n = prompt("enter a number!")
+
+let arr = []
+for (let i = 0; i < n; i++) {
+    arr[i] = i + 1;
+}
+
+let sum = arr.reduce((result, curr) => {
+    return result + curr
 })
+let factorial = arr.reduce((result, curr) => {
+    return result * curr
+})
+console.log(sum);
+console.log(factorial);
--- Javascript (by Shraddha)/Lecture 05. Functions & Methods/tempCodeRunnerFile.js
+++ Javascript (by Shraddha)/Lecture 05. Functions & Methods/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-arrFxn
+ans
