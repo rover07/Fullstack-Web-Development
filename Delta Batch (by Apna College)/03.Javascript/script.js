014adeacfd3297a5f212a6da6645baf542f21f36
Vaibhav Chauhan <vc26022003@gmail.com>
2024-11-26 23:10:56 +0530

    Updated

--- Delta Batch (by Apna College)/03.Javascript/script.js
+++ Delta Batch (by Apna College)/03.Javascript/script.js
@@ -8,3 +8,106 @@
 // let a;
 // let b = null;
 // console.log(a, "\n", b);
+
+// // // Template literals
+// let pen = 10;
+// let paper = 40;
+
+// console.log(`Total price is ${pen + paper} rupees`);
+// // truthy and falsy
+// if ("") {
+//   console.log("true");
+// } else console.log("false");
+
+//
+// // // String methods
+
+// let msg = "     string properties";
+// console.log(msg.trim());
+// console.log(msg.charAt(7));
+// console.log(msg.indexOf("st"));
+// console.log(msg.slice(5, 10));
+// console.log(msg.slice(-2));
+// console.log(msg.slice(-2));
+// console.log(msg.replace("pro", "okey"));
+// console.log(msg.repeat(2));
+
+// // method chaining
+// console.log(msg.trim().toUpperCase());
+
+// // // Arrays
+// let arr = ["apple", "banana", 78];
+// arr[0] = "watermelon";
+// arr[1] = 21;
+// arr[8] = 33;
+// console.log(arr);
+// console.log(arr[7]);
+// console.log(arr.byteLength);
+
+// // Array Q
+// let months = ["january", "july", "march", "august"];
+// let arr = ["apple", "banana", 78];
+
+// months.shift();
+// months.shift();
+// months.unshift("july", "june");
+
+// console.log(months);
+// console.log(months.includes("june"));
+// console.log(months.indexOf("august"));
+// console.log(months.concat(arr));
+// console.log(arr.reverse());
+// console.log("     ");
+
+// console.log(arr.splice(0, 2, "Hi", "Bye"));
+// console.log(arr);
+// //add
+// console.log(arr.splice(0, 0, "This is added"));
+// console.log(arr);
+// //remove
+// console.log(arr.splice(2, 1)); //removes Bye
+// console.log(arr);
+// //replace
+// console.log(arr.splice(1, 1, "Replaced element")); //Hi is replaced
+// console.log(arr);
+
+// Array Q using splice
+
+// let months = ["january", "july", "march", "august"];
+// months.splice(0, 2, "july", "june");
+// console.log(months);
+
+// // Q
+// let Q = ["c", "c++", "html", "js", "python", "java", "c#", "sql"];
+// console.log(Q.reverse().indexOf("js"));
+
+// // //Array references
+// let arr1 = [1];
+// let arr2 = [1];
+// console.log(arr1 == arr2);
+// console.log(arr1[0] == arr2[0]);
+
+// // // const array
+// const arr = [1, 2, 3];
+// arr[0] = 4;
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// // Multidimensional Arrays
+// let nums = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+// console.log(nums);
+
+// //  Q:
+// let xo = [
+//   ["X", null, "O"],
+//   ["O", null, "X"],
+//   ["O", null, " X"],
+// ];
+// console.log(xo);
+
+//
---
+++ Delta Batch (by Apna College)/03.Javascript/todo app/script.js
@@ -0,0 +1,8 @@
+// // TO-DO App
+let task = prompt("Enter task");
+let todo = [];
+while (task != "quit") {
+  let task = prompt("Enter task");
+  todo.push(task);
+}
+console.log(todo);
